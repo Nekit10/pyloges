d45d1dd2be68ef8a6743947922ac8a04328f8ac3
Nikita Serba <nikitaserba@icloud.com>
2018-09-12 22:35:53 +0200

    Add message process function

--- pyloges/logger.py
+++ pyloges/logger.py
@@ -15,3 +15,21 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Pyloges.  If not, see <https://www.gnu.org/licenses/>.
+
+import datetime
+
+
+def _process_msg(format: str, log_level: str, msg: str) -> str:
+    tt = datetime.date.today().timetuple()
+
+    new_str = format
+    new_str.replace("{level}", log_level)
+    new_str.replace("%y", tt.tm_year)
+    new_str.replace("%M", tt.tm_mon)
+    new_str.replace("%d", tt.tm_mday)
+    new_str.replace("%h", tt.tm_hour)
+    new_str.replace("%M", tt.tm_min)
+    new_str.replace("%s", tt.tm_sec)
+    new_str.replace("{msg}", msg)
+
+    return new_str
